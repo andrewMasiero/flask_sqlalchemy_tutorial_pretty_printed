4b711191f596a0d5516bc32cbd5d15b76dc0ba3b
andrewMasiero <50714278+andrewMasiero@users.noreply.github.com>
2022-11-01 20:03:21 -0500

    done with this...

--- app.py
+++ app.py
@@ -46,6 +46,3 @@ class Product(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(50), nullable=False, unique=True)
     price = db.Column(db.Integer, nullable=False)
-
-
-print('end of program')
